6f50e8bff5b66a2700d7b7d92f84e81a4da3ef07
FranciscoSimas <franciscosimas2004@hotmail.com>
2025-09-19 15:37:38 +0000

    fix: Restore original package.json to fix Vercel deployment
    - Remove monorepo structure that breaks Vercel build
    - Restore original package.json without workspaces
    - Keep all existing functionality intact
    - Prepare for alternative monorepo approach that doesn't break deployment

--- packages/shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Data exports
-export * from './data/mockData';
-export * from './data/storage';
-export * from './data/remote';
-
-// Utils exports
-export * from './utils/authUtils';
-export * from './utils/onboardingUtils';
